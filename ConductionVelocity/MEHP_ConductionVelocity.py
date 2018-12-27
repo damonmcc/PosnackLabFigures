cb3422f90f283eecd9473d96791484d3f852e4eb
Damon McCullough <damonmcc1391@gmail.com>
2018-12-27 17:13:53 -0500

    More activation maps, adjusted overall height ratios

--- ConductionVelocity/MEHP_ConductionVelocity.py
+++ ConductionVelocity/MEHP_ConductionVelocity.py
@@ -8,21 +8,21 @@ from matplotlib import ticker
 
 def example_plot(axis):
     axis.plot([1, 2])
-    axis.set_xlabel('x-label', fontsize=12)
-    axis.set_ylabel('y-label', fontsize=12)
-    axis.set_title('Title', fontsize=14)
+    # axis.set_xlabel('x-label', fontsize=12)
+    # axis.set_ylabel('y-label', fontsize=12)
+    # axis.set_title('Title', fontsize=14)
 
 
 fig = plt.figure(figsize=(8, 8))  # half 11 x 7 inch page
-gs0 = fig.add_gridspec(2, 2)  # Overall: ? row, ? columns
-gsActMaps = gs0[0].subgridspec(2, 2, hspace=0.3)  # 2 rows, 2 columns for Activation Maps
+gs0 = fig.add_gridspec(2, 2, height_ratios=[0.7, 0.3])  # Overall: ? row, ? columns
+gsActMaps = gs0[0].subgridspec(3, 2, hspace=0.3)  # 3 rows, 2 columns for Activation Maps
 gsConDealys = gs0[1].subgridspec(2, 2, hspace=0.3)  # 2 rows, 2 columns for Conduction Delays
 
 axCVMAX = fig.add_subplot(gs0[2])
 axActTimes = fig.add_subplot(gs0[3])
 
 
-for n in range(4):
+for n in range(6):
     ax = fig.add_subplot(gsActMaps[n])
     example_plot(ax)
 for n in range(4):
