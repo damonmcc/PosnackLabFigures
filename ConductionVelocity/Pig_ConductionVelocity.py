276c110c4a3dcab0bdf44a698e11442d81b90045
Damon McCullough <damonmcc1391@gmail.com>
2019-05-08 15:21:59 -0400

    Pig_CV Traces, getting trace colors from activation map pixels

--- ConductionVelocity/Pig_ConductionVelocity.py
+++ ConductionVelocity/Pig_ConductionVelocity.py
@@ -4,13 +4,15 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 from matplotlib import ticker
 from matplotlib.patches import Circle, Wedge
+import colorsys
 import seaborn as sns
 import warnings
 
 warnings.filterwarnings('ignore')
 
 # Set Colors
-roi_colors = ['0.45', '0.65', '0.85']
+# roi_colors = ['0.45', '0.65', '0.85']
+roi_colors = ['0.65', '0.65', '0.65']
 
 colorBase = 'indianred'
 colorPost = 'midnightblue'
@@ -18,10 +20,12 @@ colorsBP = [colorBase, colorPost]
 colorCTRL = 'grey'
 colorMEHP = 'black'
 exposure_colors = ['black', 'red']
-exposure_pal = sns.color_palette(exposure_colors)
 context_colors = ['0.8', '0.45']
-context_pal = sns.color_palette(context_colors)
 
+colorsROI_VmRAW = []
+colorsROI_VmRGB = []
+colorsROI_CaRAW = []
+colorsROI_CaRGB = []
 
 
 def plot_heart(axis, heart_image):
@@ -30,20 +34,33 @@ def plot_heart(axis, heart_image):
 
 def plot_ActMapVm(axis, actMap):
     axis.axis('off')
-    for idx, roi in rois.items():
+    for idx, roi in enumerate(rois):
         roi_circle = Circle((roi['x'], roi['y']), roi['r'], fc='none',
-                            ec=roi_colors[idx-1], lw=2)
+                            ec=roi_colors[idx], lw=2)
         axis.add_artist(roi_circle)
     # ros_wedge = Wedge((ros['x'], ros['y']), ros['r'], 225, 45, fc='none', ec='k', lw=2)
     # axis.add_artist(ros_wedge)
     img = axis.imshow(actMap, norm=jet_norm, cmap="jet")
-    for idx, roi in rois.items():
+    for idx, roi in enumerate(rois):
         # print('# ', idx)
-        colorRAW = img.cmap(img.norm(actMap[roi['x'], roi['y']]))
+        colorRAW = img.cmap(img.norm(actMap[roi['y'], roi['x']]))
         colorRGB = []
+        # print('ROI# ', idx, ' colorRAW: ', colorRAW)
         for idxx, norm in enumerate(colorRAW):
             colorRGB.append(int(colorRAW[idxx] * 255))
-        print('ROI# ', idx, ' colorRGB: ', colorRGB)
+
+        # Desaturate colors for plotting
+        print('* colorRAW was :', colorRAW)
+        colorHSV = colorsys.rgb_to_hsv(colorRAW[0], colorRAW[1], colorRAW[2])
+        print('colorHSV was :', colorHSV)
+        colorHSV = colorHSV[0], colorHSV[1] * 0.7, colorHSV[2]
+        print('colorHSV is  :', colorHSV)
+        colorRAW = colorsys.hsv_to_rgb(colorHSV[0], colorHSV[1], colorHSV[2])
+        print('* colorRAW is :', colorRAW)
+
+        colorsROI_VmRAW.append(colorRAW)
+        colorsROI_VmRGB.append(colorRGB)
+    print('colorsRGB: ', colorsROI_VmRGB)
     # axis.set_xlabel('x-label', fontsize=12)
     # axis.set_ylabel('y-label', fontsize=12)
     axis.set_title('Vm', fontsize=12)
@@ -52,11 +69,30 @@ def plot_ActMapVm(axis, actMap):
 
 def plot_ActMapCa(axis, actMap):
     axis.axis('off')
-    for idx, roi in rois.items():
+    for idx, roi in enumerate(rois):
         roi_circle = Circle((roi['x'], roi['y']), roi['r'], fc='none',
-                            ec=roi_colors[idx-1], lw=2)
+                            ec=roi_colors[idx], lw=2)
         axis.add_artist(roi_circle)
     img = axis.imshow(actMap, norm=jet_norm, cmap="jet")
+    for idx, roi in enumerate(rois):
+        # print('# ', idx)
+        colorRAW = img.cmap(img.norm(actMap[roi['y'], roi['x']]))
+        colorRGB = []
+        print('ROI# ', idx, ' colorRAW: ', colorRAW)
+        for idxx, norm in enumerate(colorRAW):
+            colorRGB.append(int(colorRAW[idxx] * 255))
+
+        print('* colorRAW was :', colorRAW)
+        colorHSV = colorsys.rgb_to_hsv(colorRAW[0], colorRAW[1], colorRAW[2])
+        print('colorHSV was :', colorHSV)
+        colorHSV = colorHSV[0], colorHSV[1] * 0.7, colorHSV[2]
+        print('colorHSV is  :', colorHSV)
+        colorRAW = colorsys.hsv_to_rgb(colorHSV[0], colorHSV[1], colorHSV[2])
+        print('* colorRAW is :', colorRAW)
+
+        colorsROI_CaRAW.append(colorRAW)
+        colorsROI_CaRGB.append(colorRGB)
+    print('colorsRGB: ', colorsROI_CaRGB)
     # axis.set_xlabel('x-label', fontsize=12)
     # axis.set_ylabel('y-label', fontsize=12)
     axis.set_title('Ca', fontsize=12)
@@ -94,8 +130,8 @@ def plot_TracesVm(axis, data, time_start=0.0, idx_start=None, time_window=None, 
     # Prepare axis for plotting
     axis.set_xlabel('Time (ms)', fontsize=12)
     axis.tick_params(axis='x', which='both', direction='in', bottom=True, top=False)
-    # axis.xaxis.set_major_locator(ticker.MultipleLocator(10))
-    # axis.xaxis.set_minor_locator(ticker.MultipleLocator(5))
+    axis.xaxis.set_major_locator(ticker.MultipleLocator(100))
+    axis.xaxis.set_minor_locator(ticker.MultipleLocator(20))
     axis.set_ylabel('Norm. Fluor., Vm', fontsize=12)
     axis.tick_params(axis='y', which='both', direction='in', right=False, left=True)
     axis.set_ylim([0, 1.1])
@@ -119,7 +155,7 @@ def plot_TracesVm(axis, data, time_start=0.0, idx_start=None, time_window=None, 
         data_Vm.append(trace)
         # Plot each trace
         axis.plot(times_Vm, trace,
-                  color=roi_colors[idx], linewidth=2, label='Base')
+                  color=colorsROI_VmRAW[idx], linewidth=2, label='Base')
 
 
 def plot_TracesCa(axis, data, time_start=0.0, idx_start=None, time_window=None, idx_end=None):
@@ -148,14 +184,14 @@ def plot_TracesCa(axis, data, time_start=0.0, idx_start=None, time_window=None, 
         time_start, time_window = Decimal(time_start).quantize(Decimal('.001'), rounding=ROUND_UP),\
                                   Decimal(time_window).quantize(Decimal('.001'), rounding=ROUND_UP)
         # Slice time array based on indices
-        times_Vm = times_Ca[idx_start:idx_end]
+        times_Ca = times_Ca[idx_start:idx_end]
 
     # Prepare axis for plotting
     axis.set_xlabel('Time (ms)', fontsize=12)
     axis.tick_params(axis='x', which='both', direction='in', bottom=True, top=False)
-    # axis.xaxis.set_major_locator(ticker.MultipleLocator(10))
-    # axis.xaxis.set_minor_locator(ticker.MultipleLocator(5))
-    axis.set_ylabel('Norm. Fluor., Vm', fontsize=12)
+    axis.xaxis.set_major_locator(ticker.MultipleLocator(100))
+    axis.xaxis.set_minor_locator(ticker.MultipleLocator(20))
+    axis.set_ylabel('Norm. Fluor., Ca', fontsize=12)
     axis.tick_params(axis='y', which='both', direction='in', right=False, left=True)
     axis.set_ylim([0, 1.1])
     axis.set_yticks([])
@@ -175,10 +211,11 @@ def plot_TracesCa(axis, data, time_start=0.0, idx_start=None, time_window=None, 
         # Normalize each trace
         data_min, data_max = np.nanmin(trace), np.nanmax(trace)
         trace = np.interp(trace, (data_min, data_max), (0, 1))
+        trace = 1 - trace   # Need to invert, accidentally exported as voltage??
         data_Ca.append(trace)
         # Plot each trace
-        axis.plot(times_Vm, trace,
-                  color=roi_colors[idx], linewidth=2, label='Base')
+        axis.plot(times_Ca, trace,
+                  color=colorsROI_CaRAW[idx], linewidth=2, label='Base')
 
 
 def example_plot(axis):
@@ -236,9 +273,9 @@ jet_norm = colors.Normalize(vmin=0, vmax=actMapMax)
 # Create region of interest (ROI)
 # X and Y flipped and subtracted from W and H, due to image rotation
 W, H = actMapsVm[300].shape
-rois = {1: {'y': W - 226, 'x': H - 365, 'r': 20},
-        2: {'y': W - 220, 'x': H - 276, 'r': 20},
-        3: {'y': W - 380, 'x': H - 140, 'r': 20}}
+rois = [{'y': W - 226, 'x': H - 365, 'r': 15},
+        {'y': W - 252, 'x': H - 220, 'r': 15},
+        {'y': W - 380, 'x': H - 140, 'r': 15}]
 # Create Region of Stimulation (ROS)
 ros = {'y': W - 140, 'x': H - 360, 'r': 20}
 
@@ -259,9 +296,9 @@ plot_ActMapCa(axis=axActMapsCa, actMap=actMapsCa[300])
 
 # Plot Traces
 plot_TracesVm(axis=axTraces[0], data=TraceVm[300],
-              time_start=1.15, time_window=0.6)
+              time_start=1.15, time_window=0.5)
 plot_TracesCa(axis=axTraces[1], data=TraceCa[300],
-              time_start=1.15, time_window=0.6)
+              time_start=1.15, time_window=0.5)
 
 # Fill rest with example plots
 # example_plot(axImage)
